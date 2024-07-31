ee9b089385476faeabd0b99d0548bfb34488d4ec
rajeshchary1999 <grajeshchary1929@gmail.com>
2024-07-31 11:52:58 +0530

    added files

--- models/listing.js
+++ models/listing.js
@@ -25,6 +25,12 @@ const listingSchema = new Schema({
   price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
