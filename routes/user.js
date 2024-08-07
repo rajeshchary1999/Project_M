4195e659e4e63991b0d9a3c2b6127971ed877977
rajeshchary1999 <grajeshchary1929@gmail.com>
2024-08-08 00:10:23 +0530

    files

--- routes/user.js
+++ routes/user.js
@@ -24,8 +24,13 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", "Welcome to Wanderlust!");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Wanderlust!");
+            res.redirect("/listings");
+        });
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
