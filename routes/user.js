c2e79e4cb92182b0f5c6bafe60a7d3fb0f613e5d
rajeshchary1999 <grajeshchary1929@gmail.com>
2024-08-08 00:06:02 +0530

    files

--- routes/user.js
+++ routes/user.js
@@ -46,4 +46,13 @@ router.post("/login", passport.authenticate("local", {
     res.redirect("/listings");
 });
 
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            next(err);
+        }
+        req.flash("success", "you are logged out!");
+        res.redirect("/listings");
+    })
+})
 module.exports = router;
