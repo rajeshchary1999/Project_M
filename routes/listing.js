2e20b4fcf44f00d74d715d495d0fb177deeb4f5e
rajeshchary1999 <grajeshchary1929@gmail.com>
2024-08-12 12:34:18 +0530

    files

--- routes/listing.js
+++ routes/listing.js
@@ -9,29 +9,27 @@ console.log('Listing Schema:', listingSchema); // Debugging the import
 
 const listingControllers = require("../controllers/listing.js");
 
-// Index Route
-router.get("/",
+router.route("/")
+.get(
    wrapAsync(listingControllers.index) 
-);
-
+)
+.post(isLoggedIn,  validateListing, wrapAsync(listingControllers.createlis));
 
 // New Route
 router.get("/new", isLoggedIn, listingControllers.newform);
 
 
-// Show Route
-router.get("/:id", wrapAsync(listingControllers.showlis));
+router.route("/:id")
+.get( wrapAsync(listingControllers.showlis))
+.put( isLoggedIn,isOwner, validateListing,  wrapAsync(listingControllers.updatelis))
+.delete(isOwner, wrapAsync(listingControllers.renderDelete));
 
-// Create Route
-router.post("/",isLoggedIn,  validateListing, wrapAsync(listingControllers.createlis));
 
 // Edit Route
 router.get("/:id/edit", isLoggedIn,isOwner, wrapAsync(listingControllers.renderEdit));
 
-// Update Route
-router.put("/:id", isLoggedIn,isOwner, validateListing,  wrapAsync(listingControllers.updatelis));
 
-// Delete Route
-router.delete("/:id",isOwner, wrapAsync(listingControllers.renderDelete));
+
+
 
 module.exports = router;
