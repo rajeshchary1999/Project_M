25a6af6ed3996618d47bf403c728da1a4868cb71
rajeshchary1999 <grajeshchary1929@gmail.com>
2024-08-15 21:40:26 +0530

    files

--- controllers/listing.js
+++ controllers/listing.js
@@ -13,34 +13,63 @@ module.exports.newform = (req, res) => {
 };
 
 module.exports.showlis = async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id)
-    .populate({
-      path: "reviews",
-      populate: {
-        path: "author",
-      },
-    })
-    .populate("owner")
-    if (listing && typeof listing.price === 'number') {
-      listing.formattedPrice = listing.price.toLocaleString("en-IN", {
-        style: 'currency',
-        currency: 'INR'
-      });
-    } else {
-      listing.formattedPrice = "Invalid price";
-    }
-    console.log(listing);
-    res.render("listings/show.ejs", { listing });
+  const { id } = req.params;
+  const listing = await Listing.findById(id)
+  .populate({
+    path: "reviews",
+    populate: {
+      path: "author",
+    },
+  })
+  .populate("owner");
+  
+  if (listing) {
+      console.log("Listing Coordinates:", listing.geometry.coordinates); // Debugging
+
+      if (typeof listing.price === 'number') {
+          listing.formattedPrice = listing.price.toLocaleString("en-IN", {
+              style: 'currency',
+              currency: 'INR'
+          });
+      } else {
+          listing.formattedPrice = "Invalid price";
+      }
+
+      res.render("listings/show.ejs", { listing });
+  } else {
+      req.flash("error", "Listing not found");
+      res.redirect("/listings");
+  }
 };
+
+// module.exports.showlis = async (req, res) => {
+//     const { id } = req.params;
+//     const listing = await Listing.findById(id)
+//     .populate({
+//       path: "reviews",
+//       populate: {
+//         path: "author",
+//       },
+//     })
+//     .populate("owner")
+//     if (listing && typeof listing.price === 'number') {
+//       listing.formattedPrice = listing.price.toLocaleString("en-IN", {
+//         style: 'currency',
+//         currency: 'INR'
+//       });
+//     } else {
+//       listing.formattedPrice = "Invalid price";
+//     }
+//     console.log(listing);
+//     res.render("listings/show.ejs", { listing });
+// };
 
 module.exports.createlis = async (req, res, next) => {
     let response = await geocodingClient.forwardGeocode({
       query: req.body.listing.location,
       limit: 1,
     })
-
-      .send();
+    .send();
 
     let url = req.file.path;
     let filename = req.file.filename;
@@ -54,6 +83,7 @@ module.exports.createlis = async (req, res, next) => {
     let savedListing = await newListing.save();
     console.log(savedListing);
     
+
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 };
--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,17 @@
+mapboxgl.accessToken = mapToken ;
 
-    mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 9 // starting zoom
+});
+
+
+
+const marker = new mapboxgl.Marker({ color: "red"})
+.setLngLat(listing.geometry.coordinates)
+.setPopup(new mapboxgl.Popup({offset: popupOffsets})
+.setHTML(
+    `<h4>${listing.location}</h4><p>Exact Location will be provided after booking</p`))
+.addTo(map);
+
