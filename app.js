5783a5a1c80dbaaa1f856100de40216a186ed726
rajeshchary1999 <grajeshchary1929@gmail.com>
2024-08-16 14:31:06 +0530

    files

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ store.on("error", () => {
 });
 const sessionOptions = {
   store,
-  secret: process.env.SECRET,
+  secret: process.env.SECRET, 
   resave: false,
   saveUninitalized: true,
   cookie: {
